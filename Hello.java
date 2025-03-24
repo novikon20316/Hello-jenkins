fb54997e1368b5a2d4d311e687193176569cb816
novikon20316 <109594572+novikon20316@users.noreply.github.com>
2025-03-24 09:52:32 +0200

    updated file

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 public static void main(String[] args){
 for(int i =1;i<=10;i++){
-System.out.println("Hello World"+i);
+System.out.println("Hello it's me.. i was wondering if after all the years you would like to meet? .."+i);
 }
 }
 }
